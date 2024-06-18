a2d1fd6523d25a427b32f2e83ebfb3af3c14c704
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-18 11:02:10 +0200

    feat: platform store

--- src/app/routes/beta/beta.page.ts
+++ src/app/routes/beta/beta.page.ts
@@ -1,12 +1,31 @@
-import { Component, input } from '@angular/core';
-
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject, input } from '@angular/core';
+import { PlatformStore } from '@lab/platform';
 @Component({
   standalone: true,
+  imports: [AsyncPipe, JsonPipe],
   template: `
     <h3>beta works with signal params:</h3>
     <pre>{{ q() }}</pre>
+    <p>With data from platform store: {{ data().appName }}</p>
+    <p>Events from platform store:</p>
+    <pre>{{ events$ | async | json }}</pre>
   `,
 })
 export default class BetaPage {
   q = input<string>('no params');
+  #platform = inject(PlatformStore);
+  events$ = this.#platform.events$;
+  data = this.#platform.data;
+  constructor() {
+    this.#platform.data = {
+      appName: 'CHANGED on Beta page Lab Remote Two',
+      shared: { clicked: false },
+    };
+
+    this.#platform.dispatch({
+      type: 'INIT',
+      payload: { source: 'Beta Page Remote two', date: new Date() },
+    });
+  }
 }
